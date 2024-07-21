29803037781ced7b85c4d9ff23d0d8d6edad30d4
sujiuer5201314 <15016388985a@gmail.com>
2024-07-22 01:42:43 +0800

    更新

--- C/2/2.c
+++ C/2/2.c
@@ -71,10 +71,60 @@ int main()//主函数：程序的入口  有且只有一个，而且必须要有
       float               %f
       double              %lf
     */
+    
+    //输出使用
 
     int a = 10;
-    printf("%d",a);
+    printf("%d\n",a);
+
+    char b = 'a';
+    printf("%c\n",b);
     
+    //在输出小数时默认输出后六位不够补0，且超出6位自动四舍五入
+    float c = 3.141592653589793238446f;
+    printf("%f\n",c);
+
+    float d = 3.1415f;
+    printf("%f\n",d);
+
+    double e = 3.141592653589793238446;
+    printf("%lf\n",e);
+    //"%.7lf"意为输出小数点后七位
+    printf("%.7lf\n",e);
+
+    //需要输出多个数据一次性输出
+    printf("%d\n%c\n%f\n%f\n%lf\n%.7lf\n",a,b,c,d,e);
+    printf("-------------------------\n");
+
+    //强制转换
+    int j = 'a';
+    printf("%d\n",j);
+
+    double k = (int) 'a';
+    printf("%lf\n",k);
+    
+    //输入数据
+    printf("-------------------------\n");
+    printf("请输入第一个为数字，第二三个为字母\n");
+    
+    int f;
+    char g;
+
+    //获取输入的按键
+    scanf("%d\n%c\n",&f,&g);
+    printf("-------------------\n");
+    //输出数据
+    printf("%d\n%c\n",f,g);
+
+    //分割
+    char h;
+    //获取键盘数据
+    h = getchar();
+
+    printf("-------------------\n");
+
+    //也是输出数据
+    putchar(h);
 
     return 0;//代码结束
 }
--- README.md
+++ README.md
@@ -4,8 +4,20 @@
 
 ## 这是C基础存放文件的地方！！
 
-- 1.[入门告诉你C的注意事项](https://github.com/sujiuer5201314/C/tree/main/C/1)
-- 2.[常量变量](https://github.com/sujiuer5201314/C/tree/main/C/2)
+###### 1.[入门告诉你C的注意事项](https://github.com/sujiuer5201314/C/tree/main/C/1)
+- 注释的方法
+- 引入库的方法
+- 主函数
+- 转义字符
+
+###### 2.[常量变量](https://github.com/sujiuer5201314/C/tree/main/C/2)
+- 常量
+- 变量
+- 基本数据类型
+- 占位符
+- 输出数据
+- 强制转换
+- 输入数据
 
 ## 许可证
 - [请遵守该证书，谢谢](https://github.com/sujiuer5201314/C/blob/main/README.md)
